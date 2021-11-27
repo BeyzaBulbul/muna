d5ea74739e0af387231680646ab343493e3fedb8
Beyza Bülbül <beyzahatunbulbul@hotmail.com>
2021-11-27 21:48:16 +0300

    kullanıcı modelleri oluşturuldu

---
+++ muna/models/links.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muna.models
+{
+    internal class links
+    {
+        public string previous { get; set; }
+        public string current { get; set; }
+        public string next { get; set; }
+  
+    }
+}
---
+++ muna/models/pegination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muna.models
+{
+    public class pegination
+    {
+        /*"total": 2727,
+            "pages": 137,
+            "page": 1,
+            "limit": 20,
+            "links":*/
+        public int total { get; set; }
+        public int pages { get; set; }
+        public int page { get; set; }
+        public int limit { get; set; }
+        public links links { get; set; }
+    }
+}
